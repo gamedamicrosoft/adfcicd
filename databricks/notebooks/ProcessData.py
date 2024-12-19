29b8f06c6dbf9569e84d58e6ce889eede17e1d7c
abhijeetrai <abhijeet.rai@neuroglee.com>
2024-12-19 19:04:40 +0530

    Initial Draft

--- databricks/notebooks/ProcessData.py
+++ databricks/notebooks/ProcessData.py
@@ -1,11 +1,11 @@
 # Databricks notebook source
 # Load the CSV file from Azure Blob Storage
-df = spark.read.csv("wasbs://container1@abhijeetstorage1.blob.core.windows.net/dir1/sample_adf_data.csv", header=True, inferSchema=True)
+df = spark.read.csv("wasbs://container1@gamedacicd19dec.blob.core.windows.net/dir1/sample_adf_data.csv", header=True, inferSchema=True)
 
 # Perform a simple transformation
 df_filtered = df.filter(df.Age > 30)
 
 # Write the transformed data back to Blob Storage
-df_filtered.write.csv("wasbs://container1@abhijeetstorage1.blob.core.windows.net/output/", mode="overwrite", header=True)
+df_filtered.write.csv("wasbs://container1@gamedacicd19dec.blob.core.windows.net/output/", mode="overwrite", header=True)
 
 #df_filtered.show()
